9e03667273cb19e8438d7c60067ad55a3726a4c8
fabioosnetto <fabioorione8@gmail.com>
2022-11-24 11:22:13 -0300

    New Section and Uptades
    DayTime Section - shows the current time and an image to illustrate the time;
    
    The function that sorts the sections has been shortned (displayedSections) - the array of positions was replaced by an object.

--- First-Website/TestingPage/javascript/sections.js
+++ First-Website/TestingPage/javascript/sections.js
@@ -1,105 +1,116 @@
-let posicao = {_tables : 0, _iframes : 0, _forms : 0};
+//--- Get true every new hour, after first call
+let _now = new Date();
+_now = _now.getHours();
+let nextHour = _now;
+function newHour(){
+   if(_now == nextHour){
+      nextHour = _now+1
+      return true;
+   }
+   return false;
+}
+
+//--- Object to store sections display orders
+var posicao = {
+   tables  : 0,
+   iframes : 0,
+   forms   : 0
+}
 
 function displayedSections(curr_id){
    if(curr_id == "tables"){
       if(document.getElementById(curr_id).style.display != "none"){
-         posicao._tables = 1;
-         ((posicao._iframes != 0) && (posicao._iframes != 3))? posicao._iframes += 1 : posicao._iframes = posicao._iframes; 
+         posicao.tables = 1;
+         ((posicao.iframes != 0) && (posicao.iframes != 3))? posicao.iframes += 1 : posicao.iframes = posicao.iframes; 
 
-         ((posicao._forms != 0) && (posicao._forms != 3))? posicao._forms += 1 : posicao._forms = posicao._forms;
+         ((posicao.forms != 0) && (posicao.forms != 3))? posicao.forms += 1 : posicao.forms = posicao.forms;
+
+         /*document.getElementsByTagName("html")[0].style.scrollBehavior = "smooth"
+         scrollTo(0,1000)
+         document.getElementsByTagName("html")[0].style.scrollBehavior = "auto"*/
       }
       else if(document.getElementById(curr_id).style.display == "none"){
-         posicao._tables = 0;
-         if(posicao._iframes == 2){
-            ((posicao._iframes - 1) != posicao._forms)? posicao._iframes -= 1 : posicao._iframes = posicao._iframes;
+         posicao.tables = 0;
+         if(posicao.iframes == 2){
+            ((posicao.iframes - 1) != posicao.forms)? posicao.iframes -= 1 : posicao.iframes = posicao.iframes;
           }
-          else if(posicao._iframes == 3) {
-             posicao._iframes -= 1;
+          else if(posicao.iframes == 3) {
+             posicao.iframes -= 1;
           }
          
-         posicao._forms != 1? (((posicao._forms - 1) != posicao._iframes)? posicao._forms -= 1 : posicao._forms = posicao._forms) : posicao._forms = 1;
+         if(posicao.forms != 0){
+            posicao.forms != 1? (((posicao.forms - 1) != posicao.iframes)? posicao.forms -= 1 : posicao.forms = posicao.forms) : posicao.forms = 1;
+         }
       }
    }
    else{
       if(curr_id == "iframes"){
          if(document.getElementById("iframes").style.display != "none"){
-            posicao._iframes = 1;
-            ((posicao._tables != 0) && (posicao._tables != 3))? posicao._tables += 1 : posicao._tables = posicao._tables;
+            posicao.iframes = 1;
+            ((posicao.tables != 0) && (posicao.tables != 3))? posicao.tables += 1 : posicao.tables = posicao.tables;
             
-            ((posicao._forms != 0) && (posicao._forms != 3))? posicao._forms += 1 : posicao._forms = posicao._forms;
+            ((posicao.forms != 0) && (posicao.forms != 3))? posicao.forms += 1 : posicao.forms = posicao.forms;
          }
          else if(document.getElementById("iframes").style.display == "none"){
-            posicao._iframes = 0;
-            if(posicao._tables == 2){
-              ((posicao._tables - 1) != posicao._forms)? posicao._tables -= 1 : posicao._tables = posicao._tables;
+            posicao.iframes = 0;
+            if(posicao.tables == 2){
+              ((posicao.tables - 1) != posicao.forms)? posicao.tables -= 1 : posicao.tables = posicao.tables;
             }
-            else if(posicao._tables == 3) {
-               posicao._tables -= 1;
+            else if(posicao.tables == 3) {
+               posicao.tables -= 1;
             }
             
-            posicao._forms != 1? (((posicao._forms - 1) != posicao._tables)? posicao._forms -= 1 : posicao._forms = posicao._forms) : posicao._forms = 1; 
+            if(posicao.forms != 0){
+               posicao.forms != 1? (((posicao.forms - 1) != posicao.tables)? posicao.forms -= 1 : posicao.forms = posicao.forms) : posicao.forms = 1; 
+            }
          }
       }
       else{ 
          if(curr_id == "forms"){
             if(document.getElementById("forms").style.display != "none"){
-               posicao._forms = 1;
-               ((posicao._tables != 0) && (posicao._tables != 3))? posicao._tables += 1 : posicao._tables = posicao._tables;
+               posicao.forms = 1;
+               ((posicao.tables != 0) && (posicao.tables != 3))? posicao.tables += 1 : posicao.tables = posicao.tables;
 
-               ((posicao._iframes != 0) && (posicao._iframes != 0))? posicao._iframes += 1 : posicao._iframes = posicao._iframes;
+               ((posicao.iframes != 0) && (posicao.iframes != 0))? posicao.iframes += 1 : posicao.iframes = posicao.iframes;
             }
             else if(document.getElementById("forms").style.display == "none"){
-               posicao._forms = 0;
-               if(posicao._tables == 2){
-                  ((posicao._tables - 1) != posicao._iframes)? posicao._tables -= 1 : posicao._tables = posicao._tables;
+               posicao.forms = 0;
+               if(posicao.tables == 2){
+                  ((posicao.tables - 1) != posicao.iframes)? posicao.tables -= 1 : posicao.tables = posicao.tables;
                 }
-                else if(posicao._tables == 3) {
-                   posicao._tables -= 1;
+                else if(posicao.tables == 3) {
+                   posicao.tables -= 1;
                 }
                
-               posicao._iframes != 1? (((posicao._iframes - 1) != posicao._tables)? posicao._iframes -= 1 : posicao._iframes = posicao._iframes) : posicao._iframes = 1;
+               if(posicao.iframes != 0){
+                  posicao.iframes != 1? (((posicao.iframes - 1) != posicao.tables)? posicao.iframes -= 1 : posicao.iframes = posicao.iframes) : posicao.iframes = 1;
+               }
             }
          }
       }
    }
 
-   switch (posicao._tables) {
-      case 2:
-         document.getElementById("tables").style.order = "2"
-         break;
-      case 3:
-         document.getElementById("tables").style.order = "3"
-         break;
-            
-      default:
-         document.getElementById("tables").style.order = "1"
-         break;
-   }
 
-   switch (posicao._iframes) {
-      case 2:
-         document.getElementById("iframes").style.order = "2"
-         break;
-      case 3:
-         document.getElementById("iframes").style.order = "3"
-         break;
-            
-      default:
-         document.getElementById("iframes").style.order = "1"
-         break;
-   }
+   document.getElementById("tables").style.order  = posicao.tables;
+   document.getElementById("iframes").style.order = posicao.iframes;
+   document.getElementById("forms").style.order   = posicao.forms;
 
-   switch (posicao._forms) {
-      case 2:
-         document.getElementById("forms").style.order = "2"
-         break;
-      case 3:
-         document.getElementById("forms").style.order = "3"
-         break;
-            
-      default:
-         document.getElementById("forms").style.order = "1"
-         break;
+
+   if((posicao.tables != 0) || (posicao.iframes != 0) || (posicao.forms != 0)){
+      let copyr = document.querySelector('p#copyright');
+      let time  = document.querySelector('section#itimeday');
+         
+      copyr.style.position = 'relative';
+      copyr.style.width    = '98vw';
+      time.style.bottom    = '10px';
+   }else{
+      let copyr = document.querySelector('p#copyright'); 
+      let time  = document.querySelector('section#itimeday');
+
+      copyr.style.position = 'fixed';
+      copyr.style.bottom   = '0px';
+      copyr.style.width    = '100vw'; 
+      time.style.bottom    = '40px';
    }
 }
 
@@ -113,3 +124,60 @@ function clickSections(id, display_type){
 
    displayedSections(id);
 }
+
+
+//--- That function shows current time
+function tdTime(){
+   let td_text = document.querySelector('div#itdtext'); //var to text div (where we gonna put the time)
+
+   let today = new Date(); //var to get the current date
+
+   let hour = today.getHours().toString().length > 1? today.getHours() : `0${today.getHours()}`; 
+   let min = today.getMinutes().toString().length > 1? today.getMinutes() : `0${today.getMinutes()}`;
+   let sec = today.getSeconds().toString().length > 1? today.getSeconds() : `0${today.getSeconds()}`;
+   
+   let now = `Now it's </br><strong>${hour}:${min}:${sec}</strong>`; //we only take the time
+
+   td_text.innerHTML = ''; //clean previous function call
+   td_text.innerHTML = now; //pass the time to div
+}
+
+//--- Set styles for tdImg Function
+function tdImgContent(divContainer, img_path){
+   let img = document.createElement('img');
+   img.setAttribute('src', '')
+   img.setAttribute('src', img_path)
+   divContainer.appendChild(img);
+
+   img.style.borderRadius = '50%';
+   img.style.width = '80px';
+   img.style.heigth = '80px';
+   img.style.marginTop = '10px';
+}
+
+//--- That function automatically changes the img according to a time
+function tdImg(){
+   let td_img  = document.querySelector('div#itdimg'); //var to img div
+
+   let hour = new Date();
+   hour = hour.getHours();
+
+   if((hour >= 5) && (hour < 12) && (newHour())){
+      tdImgContent(td_img, '../images/TimeDay/morning-circle.jpg');
+   }else if((hour >= 12) && (hour < 18) && (newHour())){
+      tdImgContent('src', '../images/TimeDay/afternoon-circle.jpg');
+   }else if((hour >= 18) && (hour < 24) && (newHour())){
+      tdImgContent('src', '../images/TimeDay/night-circle.jpg');
+   }else if(((hour == 24) || (hour < 5)) && (newHour())){
+      tdImgContent('src', '../images/TimeDay/dawn-circle.jpg');
+   }
+
+}
+
+//--- It calls the tdTime and tdImg functions
+function TimeDay(){
+   tdTime();
+   tdImg();
+}
+
+setInterval(function(){TimeDay()}, 500);
